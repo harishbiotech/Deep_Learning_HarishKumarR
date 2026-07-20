99433e56fb6420e66bec4bc4cf70910ab12f6ea8
harishbiotech <harishbiotechnologist@gmail.com>
2026-07-20 16:46:24 +0530

    Lab_1/*

---
+++ Lab_1/lab1_1.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+z=np.arange(-10,10,0.2)
+
+def Sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def tanh(x):
+    return np.tanh(x)
+
+def relu(x):
+    return np.maximum(0,x)
+
+def Softmax(x):
+    return np.exp(x)/np.sum(np.exp(x))
+
+A=input("Enter Sigmoid or Tanh or relu or Softmax: ")
+if A=="Sigmoid":
+    sigmoid=Sigmoid(z)
+    plt.plot(z,sigmoid)
+    plt.xlabel("z")
+    plt.ylabel("Sigmoid")
+    plt.title("Sigmoid_curve")
+    plt.grid(True)
+    plt.show()
+elif A=="Tanh":
+    Tanh=tanh(z)
+    plt.plot(z,Tanh)
+    plt.xlabel("z")
+    plt.ylabel("Tanh")
+    plt.title("Tanh_curve")
+    plt.grid(True)
+    plt.show()
+    print("The min is -1 & max is 1")
+    print('Tanh is zero centered')
+elif A=="relu":
+    relu=relu(z)
+    plt.plot(z,relu)
+    plt.xlabel("z")
+    plt.ylabel("ReLU")
+    plt.title("ReLU_curve")
+    plt.grid(True)
+    plt.show()
+    print("The min is 0 & max is 9.8")
+    print('ReLU is zero centered')
+elif A=="Softmax":
+    softmax=Softmax(z)
+    plt.plot(z,softmax)
+    plt.xlabel("z")
+    plt.ylabel("Softmax")
+    plt.title("Softmax_curve")
+    plt.grid(True)
+    plt.show()
+    print("The min is 0 & max is 0.18138")
+    print('ReLU is zero centered')
+else:
+    print("Invalid input")
